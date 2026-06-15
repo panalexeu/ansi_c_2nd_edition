d83c540e1190239ffab0d1063ea9b44cfc6f1ad6
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2026-06-15 19:27:38 +0300

    upd ex3.c

--- ch2/ex3.c
+++ ch2/ex3.c
@@ -12,9 +12,9 @@ void read_line(char []);
 int main() {
 	int res; 
 	read_line(line);
-	printf("%s\n", line);
+	printf("%s_16\n", line);
 	res = htoi(line); 
-	printf("%s_16 => %d_10", line, res);
+	printf("%d_10\n", res);
 	return 0; 
 }
 
@@ -31,37 +31,21 @@ void read_line(char s[]) {
 }
 
 int htoi(char s[]) {
-	int val, dec, strip, i, j;
-	val = dec = strip = i = j = 0;
-	int buffer[MAX_SIZE];
-	for (int k=0;k<MAX_SIZE;++k) 
-		buffer[k] = 0; 
+	int i = 0, val = 0;  
 	
 	// strip 0x or 0X chars if any 
 	if (s[0] == '0' && (s[1] == 'x' || s[1] == 'X')) 
-		i=strip=2;
+		i = 2;
 		
-	// update buffer with decimal values 
 	while (s[i] != '\0') {
 		// parse hex alphabet into decimals 
-		if (s[i] >= 'A' && s[i] <= 'F')
-			buffer[j] = s[i] - 'A' + 10; 
-		else if (s[i] >= '0' && s[i] <= '9')
-			buffer[j] = s[i] - '0'; 
-		++i; ++j; 
+		if (s[i] >= 'A' && s[i] <= 'F') s[i] = s[i] - 'A' + 10; 
+		else if (s[i] >= 'a' && s[i] <= 'f') s[i] = s[i] - 'a' + 10; 
+		else if (s[i] >= '0' && s[i] <= '9') s[i] = s[i] - '0'; 
+		// shif left by hex position, add digit
+		val = val * 16 + s[i]; 
+		++i; 
 	}
 	
-	// conversion to dec value
-	int counter = i-strip-1; 
-	for (int k=0; k<MAX_SIZE;++k) {
-		val += buffer[k] * pow(16, counter);
-		--counter;
-	} 
-
-	// debug print 
-	/*for (int j=0;j<MAX_SIZE;++j)
-		printf("%d", buffer[j]);
-	printf("\n");*/
-
 	return val;
 } 
